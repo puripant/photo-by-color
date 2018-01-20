6ca2d9fdfef96a7701ef83928765a84587b7f99e
Puripant <puripant@gmail.com>
2018-01-20 08:49:28 +0700

    use Vibrant.js to extract color and display only the matched photos

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const access_token = "";
 
+let error_margin = 0.05;
+
 var account = document.querySelector("input.input-account");
 var color = document.querySelector("input.input-color");
 var tiles = document.querySelector("#tiles");
@@ -18,13 +20,29 @@ function generate(account, color) {
   d3.json("https://www.instagram.com/" + account + "/?__a=1", function(error, userjson) {
     if (error) { return console.log(error); }
 
+    var rgb = Vibrant.Util.hexToRgb(color);
+    var hue = Vibrant.Util.rgbToHsl(rgb[0], rgb[1], rgb[2])[0];
+
     d3.json("https://api.instagram.com/v1/users/" + userjson.user.id + "/media/recent?access_token=" + access_token, function(error, photos) {
       for (var i = 0; i < photos.data.length; i++) {
-        var div = document.createElement("div");
-        div.className = "tile";
-        console.log(photos.data[i].images);
-        div.style["background-image"] = "url(" + photos.data[i].images.thumbnail.url + ")"
-        tiles.appendChild(div);
+        var url = photos.data[i].images.thumbnail.url;
+
+        var v = new Vibrant(url);
+        (function(url) {
+          v.getPalette(function(err, palette) {
+            for(var p in palette) {
+              var h = palette[p].getHsl()[0];
+              if (p !== "Vibrant" && h > hue - error_margin && h < hue + error_margin) {
+                var div = document.createElement("div");
+                div.className = "tile";
+                div.id = "tile" + i;
+                div.style["background-image"] = "url(" + url + ")"
+                tiles.appendChild(div);
+                break;
+              }
+            }
+          });
+        })(url);
       }
       // console.log(photos.pagination.next_url);
     });
